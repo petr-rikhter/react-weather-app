db4308b4ae15e79ec4d202f3ed313219f4e059c2
Petr Rikhter <petr_rikhter@mail.ru>
2023-01-05 01:39:03 +0400

    added base structure with ts

---
+++ src/pages/MonthStatisticks/components/MonthStatisticks.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./MonthStatisticks.module.scss";
+
+interface Props {}
+
+export const MonthStatisticks = (props: Props) => {
+  return <div className={styles.MonthStatisticks}>Month Statisticks</div>;
+};
